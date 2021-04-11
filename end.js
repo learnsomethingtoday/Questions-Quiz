c5a8d5a04686fddfd43258bdd9083bf4b0352294
learnsomethingtoday <56990829+learnsomethingtoday@users.noreply.github.com>
2021-04-11 11:25:11 +0530

    Update end.js

--- end.js
+++ end.js
@@ -30,7 +30,7 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/E:/Projects/Quiz App/index.html')
+    window.location.assign('/index.html')
 
     
 }
